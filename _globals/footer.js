a13652a53c2847d72a4a9fa8ac89611d82922754
jimik01 <147204488+jimik01@users.noreply.github.com>
2025-11-26 17:19:01 +0200

    Add files via upload

---
+++ _globals/footer.js
@@ -0,0 +1,6 @@
+// footer.js
+// stores the name to put into the footer copyright
+var addCopyright = true;
+var footerName = "Luschin &amp; Luschin CPA\'s, Inc.";
+if (addCopyright)
+document.write(" by " + footerName + " &#169; 2024 ");
